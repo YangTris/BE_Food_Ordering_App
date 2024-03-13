2eefa5edbfc683c6c8d29dbb91910c4c657ead90
YangTris <id.vantri911@gmail.com>
2024-03-13 13:41:41 +0700

    first commit

---
+++ src/main/java/com/example/be_food_ordering_app/BeFoodOrderingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_food_ordering_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFoodOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeFoodOrderingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/be_food_ordering_app/BeFoodOrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_food_ordering_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFoodOrderingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
