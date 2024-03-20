c59cc56c9679439b655393f877f08560b464b0ec
Dương Văn Trí <id.vantri911@gmail.com>
2024-03-20 19:34:31 +0700

    Create User.java

---
+++ src/main/java/com/example/be_food_ordering_app/model/User.java
@@ -0,0 +1,82 @@
+package com.example.be_food_ordering_app.model;
+
+public class User {
+    private String userId;
+    private String name;
+    private int roleId;
+    private String address;
+    private String phone;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userId, String name, int roleId, String address, String phone, String email,
+            String password) {
+        this.userId = userId;
+        this.name = name;
+        this.roleId = roleId;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+}
