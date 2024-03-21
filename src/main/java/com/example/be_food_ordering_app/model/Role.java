a2c50007109441365c58c3ece01181073f3c96f6
Dương Văn Trí <id.vantri911@gmail.com>
2024-03-21 23:01:11 +0700

    Create Role.java

---
+++ src/main/java/com/example/be_food_ordering_app/model/Role.java
@@ -0,0 +1,41 @@
+package com.example.be_food_ordering_app.model;
+
+public class Role {
+    private int roleId;
+    private String roleName;
+    private String description;
+
+    public Role() {
+    }
+
+    public Role(int roleId, String roleName, String description) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+        this.description = description;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
