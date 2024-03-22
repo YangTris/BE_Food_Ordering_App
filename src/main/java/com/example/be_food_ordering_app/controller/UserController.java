5c61c60ac52061bfc498d5ed345c28b30391eaf7
Dương Văn Trí <id.vantri911@gmail.com>
2024-03-22 16:30:34 +0700

    Add: UserCotroller and UserService

---
+++ src/main/java/com/example/be_food_ordering_app/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.be_food_ordering_app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.be_food_ordering_app.model.User;
+import com.example.be_food_ordering_app.service.UserService;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user")
+    public ResponseEntity<List<User>> getAllUsers() throws Exception {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserDetails(@PathVariable String id) throws Exception {
+        return ResponseEntity.ok(userService.getUserDetails(id));
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<String> createCustomer(@RequestBody User user) throws Exception {
+        return ResponseEntity.ok(userService.saveCustomer(user));
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable String id, @RequestBody User user) throws Exception {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id) {
+        return ResponseEntity.ok(userService.deleteUser(id));
+    }
+}
---
+++ src/main/java/com/example/be_food_ordering_app/service/UserService.java
@@ -0,0 +1,78 @@
+package com.example.be_food_ordering_app.service;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.stereotype.Service;
+
+import com.example.be_food_ordering_app.model.User;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+
+@Service
+public class UserService {
+
+    public List<User> getAllUsers() throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection("users").get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        List<User> users = documents.stream().map(document -> document.toObject(User.class)).toList();
+        return users;
+    }
+
+    public String saveCustomer(User user) throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+        DocumentReference docRef = db.collection("users").document();
+        user.setUserId(docRef.getId());
+        ApiFuture<WriteResult> result = docRef.set(user);
+
+        return "Saved user with ID: " + docRef.getId();
+    }
+
+    // login user by phoneNumber and password
+    public String loginUser(User user) throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection("users").whereEqualTo("phone", user.getPhone())
+                .whereEqualTo("password", user.getPassword()).get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        if (documents.isEmpty()) {
+            return null;
+        } else {
+            return documents.get(0).getId();
+        }
+    }
+
+    public User getUserDetails(String id) throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+        DocumentReference docRef = db.collection("users").document(id);
+        ApiFuture<DocumentSnapshot> future = docRef.get();
+        DocumentSnapshot document = future.get();
+        if (document.exists()) {
+            return document.toObject(User.class);
+        } else {
+            return null;
+        }
+    }
+
+    public String updateUser(String id, User user) throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+        DocumentReference docRef = db.collection("users").document(id);
+        user.setUserId(docRef.getId());
+        ApiFuture<WriteResult> result = docRef.set(user);
+
+        return "Updated user with ID: " + docRef.getId();
+    }
+
+    public String deleteUser(String id) {
+        Firestore db = FirestoreClient.getFirestore();
+        db.collection("users").document(id).delete();
+
+        return "User with ID: " + id + " has been deleted";
+    }
+}
